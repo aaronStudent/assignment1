6e85024338a32e97999ab36acda3e6c2b8d2abe4
Aaron <s3387840@student.rmit.edu.au>
2015-07-17 16:48:26 +0000

    Updating search functionality

--- answer.php
+++ answer.php
@@ -2,6 +2,15 @@
 		
 		<div id="main">
 			<h2>Answer</h2>
+			
+			The wine name is: <?php echo $_GET["wine"]; ?><br>
+			The winery name is: <?php echo $_GET["winery"]; ?><br>
+			The winery region is: <?php echo $_GET["region"]; ?><br>
+			The grape variety is: <?php echo $_GET["variety"]; ?><br>
+			The year is between: <?php echo $_GET["year1"]; ?> and <?php echo $_GET["year2"]; ?><br>
+			The stock is: <?php echo $_GET["stock"]; ?><br>
+			The order is: <?php echo $_GET["order"]; ?><br>
+			The cost is: $<?php echo $_GET["cost"]; ?> <?php echo $_GET["minmax"];?><br>
 		</div>
 		
 <?php include("layouts/footer.php"); ?>
--- search.php
+++ search.php
@@ -1,103 +1,116 @@
-<?php 
-   // Link to database login details
-   require_once('dblogin.php');
-   
-   // Create a database connection 
-   $dbconn = mysqli_connect(DB_HOST, DB_USER, DB_PW, DB_NAME);
-   
-   if(!$dbconn)
-   {
-      die('No connection (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
-   }   
-   echo 'Connected to MySQL on ' . DB_HOST . ', to Database: ' . DB_NAME .".<br/><br/><br/>\n";
-?>
 
-<?php 
+<!-- Database code -->
+
+<?php
+   // Handles database access
+   require_once('dblogin.php');
+
    // Region Database Query
    $regions  = "SELECT region.* ";
    $regions .= "FROM region";   
-   $regionResult = mysqli_query($dbconn, $regions);
-   
-   if (!$regionResult)
-   {
-      die("Database query failed while looking for region table.");
-   }
    
    // Grape Variety Database Query
    $grapes  = "SELECT grape_variety.* ";
    $grapes .= "FROM grape_variety";
-   $grapeResult = mysqli_query($dbconn, $grapes);
-   
-   if (!$grapeResult)
-   {
-      die("Database query failed while looking for grape variety table.");
-   }
-   
-   // Wine Year Database Query
-   $year  = "SELECT wine.year ";
-   $year .= "FROM wine ";
-   $year .= "ORDER BY year ASC";
+
+   // Wine Year Database Query  
+   $ascYear = $dbconn->prepare("SELECT wine.year 
+   								FROM wine 
+   								ORDER BY year ASC");
+   $ascYear->execute();
+   $minYear = $ascYear->fetchColumn();
+
+   $descYear = $dbconn->prepare("SELECT wine.year 
+   								FROM wine 
+   								ORDER BY year DESC");
+   $descYear->execute();
+   $maxYear = $descYear->fetchColumn();  
 ?>
 
 
+<!-- Start HTML --> 
+
 <?php 
-   // Include header HTML
+   // Includes header HTML
    include("layouts/header.php"); 
-?>
-		
-		<div id="main">
-			<div id="container">			
-				<h2>Winestore Database Search</h2>
-		
-				<form>
-					<fieldset id="field">
-						Wine Name: <input type="text" name="wine" id="wine"/><br>
-						Winery Name: <input type="text" name="winery" id="winery"/><br>
+?>	
+   	
+<div id="main">
+   <div id="container">			
+      <h2>Winestore Database Search</h2>
+      
+      <form action="answer.php" method="get">
+         <fieldset id="field">
+	        Wine Name: <input type="text" name="wine" id="wine"/><br>
+	        Winery Name: <input type="text" name="winery" id="winery"/><br>
 						
-						Region: <select id="region">
-                                   <?php  
-                                   $rows = mysql_num_rows($regionResult); 
-                                   $count=1;
+	        Region: 
+            <select name="region">
+               <?php
+                  $count=1;
+                  foreach ($dbconn->query($regions) as $row){
+               ?>
+               <option value="<?php echo $count ?>"><?php echo $row["region_name"]?></option>
+               <?php
+                  $count++; }               
+               ?>  
+            </select><br>
 
-								   while($row = mysqli_fetch_assoc($regionResult))
-								   {?>
-                                      <option value="<?php $count ?>"><?php echo $row["region_name"]?></option>
-								      <?php $count++;  
-								   }?>
-								</select><br>
-								
-						Grape Variety: <select id="variety">
-                                          <?php  
-                                          $rows = mysql_num_rows($grapeResult); 
-                                          $count=1;
-
-								          while($row = mysqli_fetch_assoc($grapeResult))
-								          {?>
-                                 		     <option value="<?php $count ?>"><?php echo $row["variety"]?></option>
-								 		     <?php $count++;  
-								          }?>
-									   </select><br>
-								
-					    Year: <input type="text" name="year" id="year"/><br>
-					    Minimum Stock: <input type="text" name="stock" id="stock"/><br>
-					    Minimum Order: <input type="text" name="order" id="order"/><br>
-					    Cost: $<input type="text" name="cost" id="cost"/>
-					    	  <select>
-					    	     <option value="minimum">Minimum</option>
-					    	     <option value="maximum">Maximum</option>					    	     
-					    	  </select><br></br>
+	        Grape Variety:
+            <select name="variety">
+               <option value="all">All</option>
+               <?php
+                  $count=1;
+                  foreach ($dbconn->query($grapes) as $row)
+                  {
+               ?>
+               <option value="<?php echo $count ?>"><?php echo $row["variety"]?></option>
+               <?php
+                  $count++; }               
+               ?> 
+            </select><br>
+				
+            Year:
+            <select name="year1">
+               <?php  
+                  for($i = $minYear; $i<$maxYear; $i++)
+                  { ?>
+                     <option value="<?php echo $i ?>"><?php echo $i ?></option> <?php 
+                  } 
+               ?>
+            </select>
+            to
+            <select name="year2">
+               <?php 
+                  for($i = $maxYear; $i>$minYear; $i--)
+                  { ?>
+                     <option value="<?php echo $i ?>"><?php echo $i ?></option> <?php 
+                  } 
+               ?>
+            </select><br>
+            
+	        Minimum Stock: <input type="text" name="stock" id="stock"/><br>
+	        Minimum Order: <input type="text" name="order" id="order"/><br>
+	        
+            Cost: $<input type="text" name="cost" id="cost"/>
+	               <select name="minmax">
+	                  <option value="minimum">Minimum</option>
+	                  <option value="maximum">Maximum</option>					    	     
+	               </select><br></br>
 					    
-						<input type="reset" value="Reset Form"/>				
-						<input type="button" onclick="return processOrder();" value="Process Search"/>
-					</fieldset>
-				</form>
-			</div>
-		</div>
-		
-<?php include("layouts/footer.php"); ?>
+	        <input type="submit" value="Process Search"/>
+            <input type="reset" value="Reset Form"/>				
 
-<?php 
-   //5. Close connection
-   mysqli_close($dbconn);
+         </fieldset>
+      </form>
+   </div>
+</div>
+		
+<?php
+   // Includes footer HTML 
+   include("layouts/footer.php"); 
 ?>
+
+<!-- Close database connection -->
+<?php $dbconn = null; ?>
 
