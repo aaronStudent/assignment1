fca317c2b8e861624a02e85248623c709771c9e2
Aaron <s3387840@student.rmit.edu.au>
2015-07-19 12:45:09 +0000

    Adding layouts folder

---
+++ layouts/footer.php
@@ -0,0 +1,11 @@
+<?php /* 		
+		<div id="footer">
+		Copyright 2015, Aaron Prince s3387840
+		</div> */
+?>		
+		
+	</body>	
+</html>
+
+<!-- Close database connection -->
+<?php $dbconn = null; ?>
---
+++ layouts/header.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<title>CPT375 - Assignment 1</title>
+		<link href="stylesheets/style.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+
+	<body>
+		<div id="header">
+		   <h1>Winestore</h1>
+		</div>
